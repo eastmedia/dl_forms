f3813a82593119b890777a3ecd14336cfe8b3fbb
matt <matt@14daefa2-27a9-4597-b650-bad5310c3983>
2008-02-23 10:53:12 +0000

    * Modernize for Rails 2.0 * Add additional exclusions for attributes we don't want rendering in <label> and other tags
    git-svn-id: https://svn.eastmedia.net/shared/libraries/dl_forms@5 14daefa2-27a9-4597-b650-bad5310c3983

--- dl_forms.rb
+++ dl_forms.rb
@@ -1,75 +1,7 @@
-# class LabeledBuilder < ActionView::Helpers::FormBuilder
-#   def self.create_labeled_field(method_name)
-#    define_method(%(labeled_#{method_name})) do |label, *args|
-#    @template.content_tag("label",
-#                          label.to_s.humanize,
-#                          :for => %(#{@object_name}_#{label})) +
-#      self.send(method_name.to_sym, label, *args)
-#    end
-#   end
-# 
-#   field_helpers.each do |name|
-#    create_labeled_field(name)
-#   end
-# end
-# 
-# # helpers/builder_helper.rb
-# module BuilderHelper
-#    def labeled_form_for(name, *args, &block)
-#      options = args.last.is_a?(Hash) ? args.pop : { }
-#      options = options.merge(:builder => LabeledBuilder)
-#      args = (args << options)
-#      form_for(name, *args, &block)
-#    end
-# end
-
-# <dt<%= ' class="error"' if f.object.errors.any? %>>
-#   <label for="email_address">Email</label>
-#   <span class="required">*</span>
-# </dt>
-# <% for error in f.object.errors %>
-#   <dt class="error msg"><%=h error.last %></li>
-# <% end %>
-# <dd><%= f.text_field :email, :class => "text", :maxlength => 100 %></dd>
-
 module LabeledFormHelper
-  # def secure_form_tag(*form_tag_params, &block)
-  #   @res = <<-EOF
-  #     #{form_for(*form_tag_params)} do |f|
-  #     #{capture(&block)}
-  #     #{hidden_field_tag('session_id_validation', 'asdf')}
-  #     </form>
-  #   EOF
-  #   eval '_erbout.concat @res', block
-  # end
-  
-  # [:form_for, :fields_for, :form_remote_for, :remote_form_for].each do |meth|
-  #   src = <<-end_src
-  #     def dl_#{meth}(*form_tag_params, &proc)
-  #       @res = <<-code
-  #         \#{#{meth}(*form_tag_params)} do
-  #           FOO
-  #           \#{capture(&proc)}
-  #           BAR
-  #         end
-  #       code
-  #       eval '_erbout.concat @res', proc
-  #     end
-  #   end_src
-  #   module_eval src, __FILE__, __LINE__
-  # end
-
-  # Returns a label tag that points to a specified attribute (identified by +method+) on an object assigned to a template
-  # (identified by +object+).  Additional options on the input tag can be passed as a hash with +options+.  An alternate
-  # text label can be passed as a 'text' key to +options+.
-  # Example (call, result).
-  #   label_for('post', 'category')
-  #     <label for="post_category">Category</label>
-  # 
-  #   label_for('post', 'category', 'text' => 'This Category')
-  #     <label for="post_category">This Category</label>
+  # Copied from Rails 2.0's #label
   def label_for(object_name, method, options = {})
-    ActionView::Helpers::InstanceTag.new(object_name, method, self, nil, options.delete(:object)).to_label_tag(options)
+    ActionView::Helpers::InstanceTag.new(object_name, method, self, nil, options.delete(:object)).to_label_tag(options.delete(:text), options)
   end
 
   # Creates a label tag.
@@ -81,12 +13,13 @@ module LabeledFormHelper
 end
 
 module LabeledInstanceTag
-  def to_label_tag(options = {})
-    options = options.stringify_keys
-    add_default_name_and_id(options)
-    options.delete('name')
-    options['for'] = options.delete('id')
-    content_tag 'label', options.delete('text') || @method_name.humanize, options
+  # Copied from Rails 2.0
+  def to_label_tag(text = nil, options = {})
+    name_and_id = options.dup
+    add_default_name_and_id(name_and_id)
+    options["for"] = name_and_id["id"]
+    content = (text.blank? ? nil : text.to_s) || method_name.titleize
+    content_tag("label", content, options)
   end
 end
 
@@ -97,40 +30,95 @@ module FormBuilderMethods
 end
 
 class ActionView::Helpers::FormBuilder
-  (%w(date_select) +
-   ActionView::Helpers::FormHelper.instance_methods - 
-   %w(label_for hidden_field radio_button form_for fields_for)).each do |selector|
-    src = <<-end_src
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::AssetTagHelper
+  
+  FORM_FIELDS = %w[date_select] + ActionView::Helpers::FormHelper.instance_methods - %w[label_for hidden_field radio_button form_for fields_for]
+  
+  FORM_FIELDS.each do |selector|
+    src = <<-SRC
       def dl_#{selector}(method, options = {})
         if object.respond_to?(:errors) && object.errors.respond_to?(:on) && object.errors.on(method)
-          output = @template.content_tag('dt', label_for(method, options.block(:required)) + required_label(options[:required]), :class => "error")
+          output = @template.content_tag('dt', label_for(method, options.except(:required)) + required_label(options[:required]), :class => "error")
           Array(object.errors.on(method)).each do |error|
             output += @template.content_tag('dt', error, :class => "error msg")
           end
-          output + @template.content_tag('dd', #{selector}(method, options.block(:required)), :class => "error")
+          output + @template.content_tag('dd', #{selector}(method, options.except(:required)), :class => "error")
         else
-          @template.content_tag('dt', label_for(method, options.block(:required)) + required_label(options[:required])) +
-          @template.content_tag('dd', #{selector}(method, options.block(:required)))
+          @template.content_tag('dt', label_for(method, options.except(:required, :include_blank, :height, :width, :rows, :cols, :end_year, :start_year, :order)) + required_label(options[:required]), :class => options[:class]) +
+          @template.content_tag('dd', #{selector}(method, options.except(:required, :text)), :class => options[:class])
         end
       end
-    end_src
+    SRC
     class_eval src, __FILE__, __LINE__
   end
 
-  # def radio_button(method, tag_value, options = {})
-  #   @template.content_tag('p', label_for(method) + "<br />" + super)
-  # end
+  # Similar to the dl_x form helpers but without the DL DT DD
+  FORM_FIELDS.each do |selector|
+    src = <<-SRC
+      def labeled_#{selector}(method, options = {})
+        if object.respond_to?(:errors) && object.errors.respond_to?(:on) && object.errors.on(method)
+          output = @template.content_tag(:span, label_for(method, options.except(:required)) + required_label(options[:required]), :class => "error")
+          Array(object.errors.on(method)).each do |error|
+            output += @template.content_tag(:span, error, :class => "error msg")
+          end
+          output + #{selector}(method, options.except(:required))
+        else
+          label_for(method, options.except(:required, :include_blank, :height, :width, :rows, :cols, :end_year, :start_year, :order)) + 
+          required_label(options[:required]) +
+          #{selector}(method, options.except(:required, :text))
+        end
+      end
+    SRC
+    class_eval src, __FILE__, __LINE__
+  end
+  
+  def dl_widgeditor(method, options = {})
+    widg_options = options.except(:required, :text, :height, :width).dup.merge(:class => "widgEditor")
+    style = []
+    style << "height: #{options[:height]};" if options[:height]
+    style << "width: #{options[:width]};" if options[:width]
+    widg_options[:style] = style.join(" ") unless style.blank?
+    
+    dd_content = @template.text_area(@object_name, method, widg_options)
+
+    @template.content_tag('dt', label_for(method, options.except(:required, :height, :width, :rows, :cols)) + required_label(options[:required])) + 
+    @template.content_tag('dd', dd_content)
+  end
+  
+  def dl_file_uploader(name, options = {})
+    dd_content = if (object == object.send(name) && object.respond_to?(:has_uploaded_data?) && object.has_uploaded_data?) || (object != object.send(name) && object.send(name) && object.send(name).id)
+      image_link = options[:url] ? options[:url] : @template.send(:static_image_path, object.send(name).id)
+      div_content =   @template.link_to(image_tag(image_link), image_link, :class => "image_mask") + "<span class='file_meta'>"
+      div_content +=  "File size is: " + @template.send(:h, @template.number_to_human_size(object.send(name).size)).to_s + "</span>"
+      div_content +=  @template.link_to_function @template.image_tag("icon_trash.png") + " ", <<-JS
+        $('old_#{name}').hide();
+        $('#{name}_uploader').appendChild(Builder.node('input', {
+            id:   '#{@object_name}_#{name}_uploaded_data',
+            name: '#{@object_name}[#{name}_uploaded_data]',
+            type: 'file',
+            size: '30'
+          }));
+      JS
+      @template.content_tag('div', div_content, :id => "old_#{name}")
+    else  
+      file_field("#{name}_uploaded_data", options.except(:url))
+    end
+    
+    @template.content_tag('dt', label_for(name, options.except(:url, :required)) + required_label(options[:required])) + 
+    @template.content_tag('dd', dd_content, :id => "#{name}_uploader")
+  end
   
   def dl_file_field(method, options = {})
     if object.respond_to?(:errors) && object.errors.respond_to?(:on) && object.errors.on(method)
-      output = @template.content_tag('dt', label_for(method, options.block(:required)) + required_label(options[:required]), :class => "error")
+      output = @template.content_tag('dt', label_for(method, options.except(:required)) + required_label(options[:required]), :class => "error")
       Array(object.errors.on(method)).each do |error|
         output += @template.content_tag('dt', error, :class => "error msg")
       end
-      output + @template.content_tag('dd', file_field(method, options.block(:required)), :id => 'file_field', :class => "error")
+      output + @template.content_tag('dd', file_field(method, options.except(:required)), :id => 'file_field', :class => "error")
     else
-      @template.content_tag('dt', label_for(method, options.block(:required)) + required_label(options[:required])) +
-      @template.content_tag('dd', file_field(method, options.block(:required)), :id => 'file_field')
+      @template.content_tag('dt', label_for(method, options.except(:required)) + required_label(options[:required])) +
+      @template.content_tag('dd', file_field(method, options.except(:required)), :id => 'file_field')
     end
   end
   
@@ -139,21 +127,46 @@ class ActionView::Helpers::FormBuilder
     @template.content_tag('dd', submit(*params), :class => "buttons")
   end
   
-  def dl_select(method, selections, options = {})
+  def dl_fckeditor(method, options = {})
+    options[:toolbarSet] = 'MMH'
+    dd_content = @template.fckeditor_textarea(@object_name, method, options.except(:required, :text))
+
+    @template.content_tag('dt', label_for(method, options.except(:required, :toolbarSet)) + required_label(options[:required])) + 
+    @template.content_tag('dd', dd_content)
+  end
+  
+  def dl_create_or_update_button(create_cancel_url = nil, update_cancel_url = nil, create_text = nil, update_text = nil)
+    dd_content = submit(object.new_record? ? (create_text || "Create") : (update_text || "Update"))
+    dd_content += " or "
+    
+    cancel_url = if object.new_record?
+      create_cancel_url
+    else
+      update_cancel_url
+    end
+    cancel_url ||= @template.polymorphic_url(object)
+    
+    dd_content += @template.link_to("Cancel", cancel_url)
+    
+    @template.content_tag('dt', ' ') +
+    @template.content_tag('dd', dd_content, :class => "buttons")
+  end
+  
+  def dl_select(method, selections, options = {}, html_options = {})
     if object.respond_to?(:errors) && object.errors.respond_to?(:on) && object.errors.on(method)
-      output = @template.content_tag('dt', label_for(method, options.block(:required, :include_blank)) + required_label(options[:required]), :class => "error")
+      output = @template.content_tag('dt', label_for(method, options.except(:required, :include_blank, :onchange)) + required_label(options[:required]), :class => "error")
       Array(object.errors.on(method)).each do |error|
         output += @template.content_tag('dt', error, :class => "error msg")
       end
-      output + @template.content_tag('dd', select(method, selections, options), :class => "error")
+      output + @template.content_tag('dd', select(method, selections, options, html_options), :class => "error")
     else
-      @template.content_tag('dt', label_for(method, options.block(:required, :include_blank)) + required_label(options[:required])) +
-      @template.content_tag('dd', select(method, selections, options))
+      @template.content_tag('dt', label_for(method, options.except(:required, :include_blank, :onchange)) + required_label(options[:required])) +
+      @template.content_tag('dd', select(method, selections, options, html_options))
     end
   end
 
   def dl_country_select(method, priority_countries = nil, options = {}, html_options = {})
-    @template.content_tag('dt', label_for(method, options.block(:required)) + required_label(options[:required])) +
+    @template.content_tag('dt', label_for(method, options.except(:required)) + required_label(options[:required])) +
     @template.content_tag('dd', country_select(method, priority_countries, options, html_options))
   end
 
@@ -169,7 +182,6 @@ end
 ActionView::Base.send                 :include, LabeledFormHelper
 ActionView::Helpers::InstanceTag.send :include, LabeledInstanceTag
 ActionView::Helpers::FormBuilder.send :include, FormBuilderMethods
-
 
 module ActionView
   module Helpers
@@ -189,14 +201,14 @@ module ActionView
         end
         options
       end
-
-      def tag_with_css(name, options=nil, open=false)
-        tag_without_css(name, css_options_for_tag(name, options || {}), open)
+      
+      def tag_with_css(name, options = nil, open = false, escape = true)
+        tag_without_css(name, css_options_for_tag(name, options || {}), open, escape)
       end
       alias_method_chain :tag, :css
 
-      def content_tag_string_with_css(name, content, options)
-        content_tag_string_without_css(name, content, css_options_for_tag(name, options || {}))
+      def content_tag_string_with_css(name, content, options, escape = true)
+        content_tag_string_without_css(name, content, css_options_for_tag(name, options || {}), escape)
       end
       alias_method_chain :content_tag_string, :css
     end
